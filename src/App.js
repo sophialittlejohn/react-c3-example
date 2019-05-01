3d2b6960f0f0dc57054ea0b32041fd8dd88fe7b2
sophialittlejohn <littlejohn.sophia@gmail.com>
2019-05-01 16:11:55 +0200

    Fix bug

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ import { styles } from "./utils/donut.config";
 
 const App = () => {
   return (
-    <>
-      <C3Chart
-        data={styles.data}
-        donut={styles.donut}
-        legend={styles.legend}
-        tooltip={styles.tooltip}
-        bindto="chart"
-        className="chart"
-        color={styles.color}
-      />
-    </>
+    <C3Chart
+      data={styles.data}
+      donut={styles.donut}
+      legend={styles.legend}
+      tooltip={styles.tooltip}
+      className="chart"
+      color={styles.color}
+    />
   );
 };
 
